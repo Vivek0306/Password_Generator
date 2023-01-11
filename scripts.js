38773fefe3f3b3054fc2aa830344fd502d86956c
Vivek Nair <vivekmanju53@gmail.com>
2023-01-11 13:59:54 +0530

    Changes to buttons

--- scripts.js
+++ scripts.js
@@ -65,25 +65,35 @@ copybtnDOM.addEventListener("click", () => {
 });
 
 
+function startOp(){
+  const characterAmount = lengthDOM.value;
+  const includeUppercase = uppercaseDOM.checked;
+  const includeNumbers = numbersDOM.checked;
+  const includeSymbols = symbolsDOM.checked;
+  const password = generatePassword(
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+  resultDOM.innerText = password;
+  if(resultDOM.innerText){
+    copybtnDOM.style.display = "block";
+  }
+  else{
+    copybtnDOM.style.display = "none";
+  }
+}
+
+lengthDOM.addEventListener("keypress", (e) =>{
+  if (e.key === "Enter"){
+    e.preventDefault();
+    generatebtn.click()
+  }
+});
 
 // Checking the options that are selected and setting the password
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const characterAmount = lengthDOM.value;
-    const includeUppercase = uppercaseDOM.checked;
-    const includeNumbers = numbersDOM.checked;
-    const includeSymbols = symbolsDOM.checked;
-    const password = generatePassword(
-      characterAmount,
-      includeUppercase,
-      includeNumbers,
-      includeSymbols
-    );
-    resultDOM.innerText = password;
-    if(resultDOM.innerText){
-      copybtnDOM.style.display = "block";
-    }
-    else{
-      copybtnDOM.style.display = "none";
-    }
+    e.preventDefault(); 
+    startOp();
 });
